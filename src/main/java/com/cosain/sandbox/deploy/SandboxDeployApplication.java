f5dd25e10bd4ff6c40962322bed02e32a9044d37
pia2011 <sweet_smell@nate.com>
2023-02-20 14:34:11 +0900

    init

---
+++ src/main/java/com/cosain/sandbox/deploy/SandboxDeployApplication.java
@@ -0,0 +1,13 @@
+package com.cosain.sandbox.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cosain/sandbox/deploy/SandboxDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosain.sandbox.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
